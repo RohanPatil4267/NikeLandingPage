eebc4361466d01aead0d2f4965b768edc15da85d
Rohan Patil <rohanpa4267@gmail.com>
2023-08-27 16:53:56 +0530

    Starting Files of Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import Nav from "./components/Nav";
+
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-slate-400">
-      <h1 className="text-yellow-400 text-8xl">Hello World</h1>
-    </div>
+    <main className="relative">
+      <Nav/>
+      <section className="xl:padding-l wide:padding-r padding-+b">
+        Hero
+      </section>
+      <section className="padding">
+        PopularProducts
+      </section>
+      <section className="padding">
+        SuperQuality
+      </section>
+      <section className="padding">
+        Services
+      </section>
+      <section className="padding">
+        SpecialOffers
+      </section>
+      <section className="padding bg-pale-blue">
+        CustomerReviews
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="padding-x bg bg-black padding-t pb-8">
+        Footer
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import { headerLogo } from "../Assets/images";
+import { hamburger } from "../Assets/icons";
+
+function Nav() {
+  return (
+    <header>navigation</header>
+  );
+}
+
+export default Nav;
