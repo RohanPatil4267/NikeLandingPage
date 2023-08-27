fa37d8ddf708d35194ea037906c0d8131f054dfa
Rohan Patil <rohanpa4267@gmail.com>
2023-08-27 15:52:49 +0530

    Starting Files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-slate-400">
+      <h1 className="text-yellow-400 text-8xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
